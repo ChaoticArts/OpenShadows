a8a255b1e4e185cf5b70b71347ff52bcb0876d08
CAMongrel <camongrel@gmail.com>
2022-12-12 20:08:52 +0100

    Add some comments to OffExtractor

--- Src/OpenShadows.FileFormats/Images/OffExtractor.cs
+++ Src/OpenShadows.FileFormats/Images/OffExtractor.cs
@@ -10,6 +10,26 @@ namespace OpenShadows.FileFormats.Images
 {
 	public static class OffExtractor
 	{
+		/// <summary>
+		/// OFF files are always a multiple of 26 bytes in length and always have a matching
+		/// .NVF file associated with it. The .NVF contains n images which directly correlate 
+		/// to the entries in the matching OFF file. 
+		/// Additionally there is a matching full screen battle image associated.
+		/// Example:
+		/// QU_PART.NVF has two images in it
+		/// QU_PART.OFF has two 26 byte entries in it (52 bytes in total)
+		/// QU_BACK.AIF is the whole battle screen.
+		/// 
+		/// The OFF entries somehow define the location of the object on the corresponding 
+		/// battle screen.
+		/// 
+		/// Theory:
+		/// The data in the OFF does not match to pixels, but it may match to the cells in
+		/// the battle screen (unconfirmed).
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		/// <exception cref="InvalidDataException"></exception>
 		public static List<OffsetData> ExtractOffsets(byte[] data)
 		{
 			if (data.Length % 26 != 0)
